ccce3d7b374be6c8cf8b2e95fb8187ee621f14f3
Sahil Jartare <tup19446@temple.edu>
2025-09-21 02:21:14 -0400

    Implemented Generate feature

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,5 +1,5 @@
 import styles from "./App.module.css";
-import { useRef, useState } from "react";
+import { use, useRef, useState } from "react";
 
 import Navbar from "./Navbar";
 import type { DrawingHandle } from "./Drawing";
@@ -12,13 +12,50 @@ export enum Page {
 }
 
 export default function App() {
+  //Represents which pagte is currently shown.
   const [currentPage, setCurrentPage] = useState(Page.Drawing);
+
+  //Reference to the Drawing component.
   const drawingRef = useRef<DrawingHandle | null>(null);
 
+  //Holds the HTML returned by the backend so we can render it on the Design page
+  const [html, setHtml] = useState<string>("");
+
+  //Called when user clicks the "Generate" button in the Navbar.
   const handleGenerate = async () => {
-    // Use the built-in exporter wrapped inside Drawing.
-    await drawingRef.current?.exportPNGInNewTab?.();
-  };
+    const blob = await drawingRef.current?.getPNGBlob?.();
+    if (!blob) {
+      alert("Please create a sketch first. No sketch to export");
+      return;
+    }
+  //form data to send to backend
+  const sketch = new FormData();
+  sketch.append("sketch", blob, "sketch.png");
+
+  const res = await fetch("/api/generate/", {
+    method: "POST",
+    body: sketch,
+  });
+
+  if (!res.ok) {
+    alert("Failed to generate HTML from sketch");
+    return;
+  }
+
+  //Export JSON back from the server
+  const data = (await res.json()) as {html?: string};
+  const htmlStr = (data.html ?? "").trim();
+  if (!htmlStr) {
+    alert("No HTML received from server");
+    return;
+  }
+  //Save HTML
+  setHtml(htmlStr);
+
+  //Here you can set the current page to Mockup if you want to switch automatically
+  //setCurrentPage(Page.Mockup); 
+
+  }
 
   return (
     <div className={styles.appRoot}>
